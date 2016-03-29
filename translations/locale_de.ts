af46be3ed481914c8c3e29b8a5ee986c60cf6a3a
Joachim Schmitz <jojo@schmitz-digital.de>
2016-03-29 21:57:07 +0200

    add German translation source

---
+++ translations/locale_de.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Ms::MuseScore</name>
+    <message>
+        <location filename="../TempoChanges.qml" line="+185"/>
+        <source>Staff Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrefsDialogBase</name>
+    <message>
+        <location line="+64"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location line="-221"/>
+        <source>No score open.
+This plugin requires an open score to run.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TempoChanges</name>
+    <message>
+        <location line="-9"/>
+        <source>Creates linear hidden tempo markers.
+See also: https://musescore.org/en/handbook/tempo-0#ritardando-accelerando</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+175"/>
+        <source>BPM beat: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Start BPM: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>End BPM: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
