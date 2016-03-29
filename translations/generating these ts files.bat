cd17b67a2594ff739a2578055034df09e4dccbc3
Joachim Schmitz <jojo@schmitz-digital.de>
2016-03-29 21:55:22 +0200

    set PATH so the the batch files find lupdate/lrelease

--- translations/generating these ts files.bat
+++ translations/generating these ts files.bat
@@ -1,3 +1,4 @@
 REM add more local_xx.ts files to this string when required
+set PATH=%PATH%;C:\Qt\5.6\mingw49_32\bin;C:\Qt\5.4\mingw491_32\bin
 lupdate.exe -locations relative -no-obsolete ../ -ts locale_nl.ts
 PAUSE
