fa339e38591db27a5d2a6b966799119c3b60e8ef
Joachim Schmitz <jojo@schmitz-digital.de>
2019-03-18 12:29:43 +0100

    Add French translation by JLWaltener

--- TempoChanges.qml
+++ TempoChanges.qml
@@ -1,10 +1,10 @@
 //=============================================================================
 //  TempoChanges Plugin
 //
-//  Based on the principle of hidden tempo markings mentioned in the 2.0 handbook
+//  Based on the principle of hidden tempo markings mentioned in the handbook
 //  Attempts to create a linear ritartando or accelerando
 //
-//  Copyright (C) 2016 Johan Temmerman (jeetee)
+//  Copyright (C) 2016-2019 Johan Temmerman (jeetee)
 //=============================================================================
 import QtQuick 2.2
 import QtQuick.Controls 1.1
@@ -15,7 +15,7 @@ import MuseScore 1.0
 
 MuseScore {
       menuPath: "Plugins.TempoChanges"
-      version: "2.3.1"
+      version: "2.3.2"
       description: qsTr("Creates linear hidden tempo markers.\nSee also: https://musescore.org/en/handbook/tempo-0#ritardando-accelerando")
       pluginType: "dialog"
       //requiresScore: true //not supported before 2.1.0, manual checking onRun
---
+++ translations/locale_fr.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>Ms::MuseScore</name>
+    <message>
+        <location filename="../TempoChanges.qml" line="+28"/>
+        <source>No score open.
+This plugin requires an open score to run.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+157"/>
+        <source>Staff Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrefsDialogBase</name>
+    <message>
+        <location line="+64"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TempoChanges</name>
+    <message>
+        <location line="-230"/>
+        <source>Creates linear hidden tempo markers.
+See also: https://musescore.org/en/handbook/tempo-0#ritardando-accelerando</source>
+        <translation>Crée une ligne d&apos;indications de tempo cachés.
+Voir: https://musescore.org/fr/handbook/tempo#ritardando-accelerando</translation>
+    </message>
+    <message>
+        <location line="+175"/>
+        <source>BPM beat:</source>
+        <translation>Temps BPM:</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Start BPM:</source>
+        <translation>BPM de début:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>End BPM:</source>
+        <translation>BPM de fin:</translation>
+    </message>
+</context>
+</TS>
