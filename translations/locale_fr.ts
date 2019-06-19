374138418a0a2bf8275f5518becbec930b49d54e
JLWALTENER <jeanlouis.waltener@free.fr>
2019-06-19 10:56:04 +0200

    Updaate French Translation

--- translations/locale_fr.ts
+++ translations/locale_fr.ts
@@ -23,9 +23,9 @@
         <location line="-450"/>
         <source>Creates hidden tempo markers.
 See also: https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</source>
-        <translatorcomment>Handbook not yet translated into Frech</translatorcomment>
-        <translation>Crée d&apos;indications de tempo cachés.
-Voir: https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</translation>
+        <translatorcomment>Handbook not yet translated into French.</translatorcomment>
+        <translation>Crée des indications de tempo cachés.
+Voir aussi : https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</translation>
     </message>
     <message>
         <location line="+11"/>
@@ -33,39 +33,39 @@ Voir: https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</translat
 TempoChanges needs v3.0.5 or above.
 </source>
         <translation>Version non supportée de MuseScore.
-TempoChanges fonctionne à partir de la v3.0.5 et supérieure.
+TempoChanges fonctionne à partir de la v3.0.5 et ultérieure.
 </translation>
     </message>
     <message>
         <location line="+288"/>
         <source>BPM beat:</source>
-        <translation>Temps BPM:</translation>
+        <translation>Temps BPM :</translation>
     </message>
     <message>
         <location line="+35"/>
         <source>Start BPM:</source>
-        <translation>BPM de début:</translation>
+        <translation>BPM de début :</translation>
     </message>
     <message>
         <location line="+11"/>
         <source>End BPM:</source>
-        <translation>BPM de fin:</translation>
+        <translation>BPM de fin :</translation>
     </message>
     <message>
         <location line="+13"/>
         <location line="+6"/>
         <source>Linear</source>
-        <translation type="unfinished"></translation>
+        <translation>Linéaire</translation>
     </message>
     <message>
         <location line="-5"/>
         <source>Curved</source>
-        <translation type="unfinished"></translation>
+        <translation>Incurvé</translation>
     </message>
     <message>
         <location line="+13"/>
         <source>midpoint:</source>
-        <translation type="unfinished"></translation>
+        <translation>Point milieu :</translation>
     </message>
 </context>
 </TS>
