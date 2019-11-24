6beca8019e4e3cc78d496009e032215a6d7f387a
jeetee <github@jeetee.net>
2019-11-24 22:47:40 +0100

    Italian translation, courtesy of Shoichi (https://musescore.org/en/user/44016)

--- translations/generating these ts files.bat
+++ translations/generating these ts files.bat
@@ -1,7 +1,8 @@
 REM add more local_xx.ts files to this string when required
-set PATH=%PATH%;C:\Qt\5.12.2\msvc2017_64\bin
+set PATH=%PATH%;C:\Qt\5.12.5\msvc2017_64\bin
 lupdate.exe -locations relative -no-obsolete ../ -ts locale_de.ts
 lupdate.exe -locations relative -no-obsolete ../ -ts locale_fr.ts
+lupdate.exe -locations relative -no-obsolete ../ -ts locale_it.ts
 lupdate.exe -locations relative -no-obsolete ../ -ts locale_nl.ts
 lupdate.exe -locations relative -no-obsolete ../ -ts locale_zh.ts
 PAUSE
---
+++ translations/locale_it.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>Ms::MuseScore</name>
+    <message>
+        <location filename="../TempoChanges.qml" line="+231"/>
+        <source>Staff Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrefsDialogBase</name>
+    <message>
+        <location line="+240"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TempoChanges</name>
+    <message>
+        <location line="-450"/>
+        <source>Creates hidden tempo markers.
+See also: https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</source>
+        <translatorcomment>Handbook not yet translated into Italian.</translatorcomment>
+        <translation>Crea indicazioni metriche nascoste.
+Vedi anche : https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Unsupported MuseScore version.
+TempoChanges needs v3.0.5 or above.
+</source>
+        <translation>Versione non supportata di MuseScore.
+TempoChanges richiede la versione 3.0.5 o superiore.
+</translation>
+    </message>
+    <message>
+        <location line="+288"/>
+        <source>BPM beat:</source>
+        <translation>Battito BPM :</translation>
+    </message>
+    <message>
+        <location line="+35"/>
+        <source>Start BPM:</source>
+        <translation>BPM iniziale :</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>End BPM:</source>
+        <translation>BPM finale :</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <location line="+6"/>
+        <source>Linear</source>
+        <translation>Lineare</translation>
+    </message>
+    <message>
+        <location line="-5"/>
+        <source>Curved</source>
+        <translation>Curva</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>midpoint:</source>
+        <translation>Punto medio :</translation>
+    </message>
+</context>
+</TS>
