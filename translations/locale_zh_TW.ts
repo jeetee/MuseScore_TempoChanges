411176b8fdba9584d4666e6c1ea581c2920801fa
sunny00217wm <59136112+sunny00217wm@users.noreply.github.com>
2020-05-10 00:36:37 +0200

    Squashed commit of the following:
    commit 8331bdbd7976d02b61345ec49bed69db15e1e053
    Author: sunny00217wm <59136112+sunny00217wm@users.noreply.github.com>
    Date:   Tue Apr 28 22:39:28 2020 +0800
    
        Add files via upload
    
    commit fbc6cea609fa9ff1d60335814e3bb4c5107d2673
    Author: sunny00217wm <59136112+sunny00217wm@users.noreply.github.com>
    Date:   Tue Apr 28 22:37:22 2020 +0800
    
        Add files via upload
    
    commit b1aa0328c842b4d8e60f511b897b0aed5aa8a95c
    Author: sunny00217wm <59136112+sunny00217wm@users.noreply.github.com>
    Date:   Tue Apr 28 22:34:56 2020 +0800
    
        Create locale_zh_TW.ts

---
+++ translations/locale_zh_TW.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh">
+<context>
+    <name>Ms::MuseScore</name>
+    <message>
+        <location filename="../TempoChanges.qml" line="+325"/>
+        <source>Staff Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrefsDialogBase</name>
+    <message>
+        <location line="+256"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TempoChanges</name>
+    <message>
+        <location line="-560"/>
+        <source>Creates hidden tempo markers.
+See also: https://musescore.org/en/handbook/3/tempo#ritardando-accelerando</source>
+        <translation>建立隱藏的速度標記。
+參見：https://musescore.org/zh-hant/node/278604#ritardando-accelerando</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Unsupported MuseScore version.
+TempoChanges needs v3.0.5 or above.
+</source>
+        <translation>您使用的MuseScore版本過低（低於3.0.5），因此本插件無法運作！。
+</translation>
+    </message>
+    <message>
+        <location line="+381"/>
+        <source>BPM beat:</source>
+        <translation>BPM類型：</translation>
+    </message>
+    <message>
+        <location line="+51"/>
+        <source>Start BPM:</source>
+        <translation>起始BPM：</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>End BPM:</source>
+        <translation>結束BPM：</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <location line="+6"/>
+        <source>Linear</source>
+        <translation>線性</translation>
+    </message>
+    <message>
+        <location line="-5"/>
+        <source>Curved</source>
+        <translation>曲線</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>midpoint:</source>
+        <translation>中間點：</translation>
+    </message>
+</context>
+</TS>
